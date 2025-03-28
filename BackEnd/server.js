9ed579c1c0c7afacd451a30572e47d815bac5a5b
Tharun0730 <tharunsankari007@gmail.com>
2025-03-28 23:54:27 +0530

    Added the backend and api for auth,employee api's

---
+++ BackEnd/server.js
@@ -0,0 +1,18 @@
+const express=require('express')
+const dotenv=require('dotenv')
+const cors=require('cors')
+const db=require('./db')
+const authRoutes = require("./routes/authRoutes");
+const employee = require("./routes/employeeRoutes");
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+app.use("/employee",employee)
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`🚀 Server running on port ${PORT}`);
+});
